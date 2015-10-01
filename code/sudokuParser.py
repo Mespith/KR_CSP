f63891b3809b894b807761b32184029c0e7f83c9
David_Zomerdijk <david.zomerdijk93@gmail.com>
2015-10-01 10:39:59 +0200

    added constraint class

--- code/sudokuParser.py
+++ code/sudokuParser.py
@@ -1,4 +1,5 @@
 import variable
+import constraint
 import math
 #This script parses sudokus from an input file into variables with domains.
 #Input:
@@ -8,27 +9,45 @@ import math
 oneLine = ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8............942.8.16.....29........89.6.....14..25......4.......2...8.9..5....7.."
 size = 0
 
-def ParseLine(line):
-    sudoku = []
+#this def is right from: http://stackoverflow.com/questions/1265665/python-check-if-a-string-represents-an-int-without-using-try-except
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def ParseLine(line,size):
+    #the variables of the sudoku
+    variables = []
+    constraints = []
+    count = 0
     for unit in line:
         if unit == '.':
             #Create a variable with a full domain.
-            sudoku.append(variable.Variable(range(1, size+1)))
-        else:
-            #Create a variable with its number as the only value in the domain.
-            sudoku.append(variable.Variable([int(unit)]))
-    return sudoku
+            variables.append(variable.Variable(range(1, size+1)))
+        elif( RepresentsInt( unit ) ):
+            #Create a variable with a full domain
+            variables.append(variable.Variable(range(1, size+1)))
+            #create a constraint
+            new_constraint =  constraint.constraint( count )
+            new_constraint.domain_constraint = [1 , 2, 3, 4, 5, 6, 7, 8, 9].remove(int(unit))
+            constraints.append (new_constraint)
+        count = count + 1
     
+    return [variables, constraints]
+   
+
 def ParseFile(filePath):
     sudokus = []
     #Open the file.
     sudokuFile = open(filePath, 'r')
     for line in sudokuFile:
-        #Save the dimension of the sudoku (it is the size of the domain)
-        if size == 0:
-            size = math.sqrt(len(line))
-        sudokus.append(ParseLine(line))
+        size = int (math.sqrt(len(line) -1) )
+        sudokus.append(ParseLine(line,size))
     return sudokus
     
-results = ParseFile("100 sudokus.txt")
-print(results)
+results = ParseFile("../1000 sudokus.txt")
+
+print results[0][0][0].domain
