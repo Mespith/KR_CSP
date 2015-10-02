9c27ad4686ef0277f1f58fc2a690691f2bfb95b2
Jaimy <jaimyTvandijk@gmail.com>
2015-10-02 12:51:31 +0200

    Verder met k consistency

--- code/constraintPropagation.py
+++ code/constraintPropagation.py
@@ -1,4 +1,4 @@
-import intertools
+import itertools
 #This script describes the constraint propagation.
 #This can be done in different ways:
 #- Reduce a Domain
@@ -12,6 +12,6 @@ def consistency(k, CSP, constraints):
     
     for i in range(1, k+1):
         #create i-tuples of N
-        
-def i_tuples(variables, i):
-    
+        Ni = itertools.combinations(CSP, i)
+        for ni in Ni:
+            
