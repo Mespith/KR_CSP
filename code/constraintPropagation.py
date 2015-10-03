9f9240b45e29cfbfd9ed6feea3d0c98ce1eb06b1
Jaimy <jaimyTvandijk@gmail.com>
2015-10-03 14:17:35 +0200

    Bezig met propagation testen.

--- code/constraintPropagation.py
+++ code/constraintPropagation.py
@@ -1,35 +1,87 @@
 import itertools
+import variable
+import constraint
+import math
 #This script describes the constraint propagation.
 #This can be done in different ways:
 #- Reduce a Domain
 #- Reduce Constraints (Usually by introducing new constraints)
 
+oneLine = ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8............942.8.16.....29........89.6.....14..25......4.......2...8.9..5....7.."
+
+def ParseLine(line,size):
+    #the variables of the sudoku
+    variables = []
+    constraints = []
+    count = 0
+    for unit in line:
+        if unit == '.':
+            #Create a variable with a full domain.
+            variables.append(variable.Variable(range(1, size+1)))
+        elif( RepresentsInt( unit ) ):
+            #Create a variable with a full domain
+            variables.append(variable.Variable(range(1, size+1)))
+            #create a constraint
+            constraint_list = [1 , 2, 3, 4, 5, 6, 7, 8, 9] 
+            constraint_list.remove (int(unit) )
+            for x in constraint_list:
+                new_constraint =  constraint.constraint( variables[ len(variables) - 1 ] )
+                new_constraint.unary_constraint = x
+                constraints.append (new_constraint)
+        count = count + 1
+
+    sudoku_constraints = general_sudoku_constraints( variables )
+    
+    return [variables, constraints]
+    
+def RepresentsInt( s ):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+        
+def general_sudoku_constraints( variables ):
+    size = int (math.sqrt(len(variables) )) 
+    constraints = []
+    #constraint for every number in every column the number cannot be equal
+    for a in range (0, size):
+        row = variables[ size * a : size * a + size]
+        for i in row:
+            for j in row:
+                if(i != j):
+                    new_constraint =  constraint.constraint( i )
+                    new_constraint.variable2 = j
+                    constraints.append( new_constraint )
+    
+result = ParseLine(oneLine, 9)
+CSP = result[0]
+constraints = result[1]
+
 def consistency(k, CSP, constraints):
     #Initialization
     someList = []
     R = list(constraints)
-    M = 0
+    M = dict()
     
     for i in range(1, k+1):
         #create i-tuples of N
         Ni = itertools.combinations(CSP, i)
-        print('Ni: ', Ni)
         for ni in Ni:
-            print('ni: ', ni)
             Li = i_tupleL(ni, i)
-            print('Li: ', Li)
             for li in Li:
-                print('li: ', li)
                 if not CheckRelations(ni, li, constraints):
-                    print('False')
                     someList.append((ni, li, i))
-                    M[ni, li] = 1
+                    M[(ni, li)] = 1
 
 def i_tupleL(variables, i):
     domains = []
     for var in variables:
         domains.append(var.domain)
-    return itertools.product(domains, repeat=i)
+    if i == 1:
+        return itertools.combinations(domains[0], i)
+    else:
+        return itertools.product(domains, repeat=i)
     
 def CheckRelations(variables, values, constraints):
     for constraint in constraints:
@@ -46,3 +98,5 @@ def CheckRelations(variables, values, constraints):
                 if value1 == value2:
                     return False
     return True
+    
+consistency(1, CSP, constraints)
