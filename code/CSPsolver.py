36af04f8d1b78a1c340ea0f56c085ecec5742b70
Jaimy <jaimyTvandijk@gmail.com>
2015-10-06 14:27:01 +0200

    Het basis algoritme werkt

--- code/CSPsolver.py
+++ code/CSPsolver.py
@@ -3,29 +3,27 @@ import validation
 import arc_consistency
 #This script is the general CSP solving algorithm.
         
-def Solve(variables, constraints, depth):
-    #print('Solving CSP on level: ' + str(depth))
+def Solve(variables, unary_constraints, constraints, count):
+    if count == 0:
+        arc_consistency.unary_reduce(variables, unary_constraints)
     #Propagate the constraints. If false, then it is unsatifiable
-    #print('Propagating constraints')
     satisfiable = arc_consistency.AC_3(variables, constraints)
     if not satisfiable:
-        #print('This CSP is insatifiable.')
-        return (False, variables)
+        return (False, variables, count)
     #Check if problem is solved
-    #print('Checking if we found a solution.')
     solved = validation.Happy(variables)
     if solved:
-        #print('We did!')
-        return (True, variables)
+        return (True, variables, count)
     else:
-        #print('We did not. Creating sub-problems now.')
         #Create sub-problems
         CSP_1, CSP_2 = split.Split(variables)
         #Solve the sub-problems
-        result = Solve(CSP_1, constraints, depth+1)
+        result = Solve(CSP_1, unary_constraints, constraints, count+1)
         if result[0]:
             return result
-        result = Solve(CSP_2, constraints, depth+1)
+        result = Solve(CSP_2, unary_constraints, constraints, result[2]+1)
         if result[0]:
             return result
-    return (False, variables) 
+        else:
+            count = result[2]
+    return (False, variables, count)
--- code/arc_consistency.py
+++ code/arc_consistency.py
@@ -1,32 +1,33 @@
 from collections import deque
 
 def AC_3(variables, constraints):
-    for x in variables:
-        # Make consistent with unary constraints.
-        unary_reduce(x, constraints)
-    
     #Create the queue for checking constraints
     queue = deque(constraints)
     
     while len(queue) > 0:
         r = queue.popleft()
         if not r.unary_constraint:
-              if arc_reduce(r.variable1, r.variable2):
-                  if len(r.variable1.domain) == 0:
-                      return False
-                  elif len(r.variable1.domain) == 1:
-                      # Add all the constraints to the queue where x is the second variable.
-                      # This is only usefull if x is reduced to 1 value.
-                      for con in constraints:
-                          if con.variable2 == r.variable1:
-                              queue.append(con)  
+            x = variables[r.variable1]
+            y = variables[r.variable2]
+            if arc_reduce(x, y):
+                if len(x.domain) == 0:
+                    return False
+                elif len(x.domain) == 1:
+                    # Add all the constraints to the queue where x is the second variable.
+                    # This is only useful if x is reduced to 1 value.
+                    for con in constraints:
+                        if con.variable2 == r.variable1:
+                            queue.append(con)
     return True                              
         
-def unary_reduce(x, constraints):
-    for r in constraints:
-        if r.variable1 == x and r.unary_constraint:
-            if r.unary_constraint in x.domain:
-                x.domain.remove(r.unary_constraint)
+def unary_reduce(variables, constraints):
+    for i in range(len(variables)):
+        # Make consistent with unary constraints.
+        for r in constraints:
+            if r.variable1 == i and r.unary_constraint:
+                x = variables[i]
+                if r.unary_constraint in x.domain:
+                    x.domain.remove(r.unary_constraint)
             
 def arc_reduce(x, y):
     change = False    
