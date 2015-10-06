2ed318d5cebb4bfc86aedb3d471fa83a3149d9b2
Jaimy <jaimyTvandijk@gmail.com>
2015-10-06 15:13:56 +0200

    Volgens mij kan satisfied_constraints gebruikt worden.

--- code/CSPsolver.py
+++ code/CSPsolver.py
@@ -3,11 +3,11 @@ import validation
 import arc_consistency
 #This script is the general CSP solving algorithm.
         
-def Solve(variables, unary_constraints, constraints, count):
+def Solve(variables, unary_constraints, constraints, count, satisfied_constraints):
     if count == 0:
         arc_consistency.unary_reduce(variables, unary_constraints)
     #Propagate the constraints. If false, then it is unsatifiable
-    satisfiable = arc_consistency.AC_3(variables, constraints)
+    satisfiable, satisfied_constraints = arc_consistency.AC_3(variables, constraints, satisfied_constraints)
     if not satisfiable:
         return (False, variables, count)
     #Check if problem is solved
@@ -18,10 +18,10 @@ def Solve(variables, unary_constraints, constraints, count):
         #Create sub-problems
         CSP_1, CSP_2 = split.Split(variables)
         #Solve the sub-problems
-        result = Solve(CSP_1, unary_constraints, constraints, count+1)
+        result = Solve(CSP_1, unary_constraints, constraints, count+1, satisfied_constraints)
         if result[0]:
             return result
-        result = Solve(CSP_2, unary_constraints, constraints, result[2]+1)
+        result = Solve(CSP_2, unary_constraints, constraints, result[2]+1, satisfied_constraints)
         if result[0]:
             return result
         else:
